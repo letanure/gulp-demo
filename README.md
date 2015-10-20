321d87d958e34c76d4c4fc45981feff213155cdc
letanure <letanure@gmail.com>
2015-10-20 13:46:49 +0200

    Add gulp build scripts

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # gulp-demo
+
+## Gulp
+
+### Scripts
+
+Concatenate all files with extension ```.js``` to the ```app-build.js```on ```build/```
+
+```bash
+$ gulp build-scripts
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,25 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+  	gulpConcat = require('gulp-concat');
+
+var config = {
+  source: {
+    scripts: {
+      path: 'app/',
+      files: './app/**/*.js'
+    }
+  },
+  build: {
+    scripts: 'build/scrips/'
+  }
+
+}
+
+gulp.task('build-scripts', function() {
+  return gulp.src(config.source.scripts.files)
+    .pipe(gulpConcat('app-build.js'))
+    .pipe(gulp.dest(config.build.scripts));
+
+});
 
 gulp.task('default', function() {
   // place code for your default task here
